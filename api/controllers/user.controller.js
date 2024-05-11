d2e8ccc97b26cb958cef5289a537d2052b958d3b
EnzoPa70 <enzo.angular50@gmail.com>
2024-05-12 01:34:03 +0200

    Add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -16,7 +16,7 @@ export const updateUser = async (req, res, next) => {
   }
   try {
     if (req.body.password) {
-      req.body.password = await bcryptjs.hashSync(req.body.password, 10);
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
     const updatedUser = await User.findByIdAndUpdate(
@@ -37,3 +37,18 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+// delete user
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, 'You can delete only your account!'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted...');
+  } catch (error) {
+    next(error);
+  }
+
+}
