cd875e53e627f1c89951dcb656fbff0a064d37ae
EnzoPa70 <enzo.angular50@gmail.com>
2024-05-12 11:51:28 +0200

    add pre production changes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -45,6 +45,7 @@ export default function Profile() {
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setImagePercent(Math.round(progress));
       },
+      
       // eslint-disable-next-line no-unused-vars
       (error) => {
         setImageError(true);
